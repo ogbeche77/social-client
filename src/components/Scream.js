3718bbc55f6b5025cf1f0f3c0760e6ac1a92b4e5
ogbeche77 <ogbeche77@yahoo.com>
2020-06-21 17:46:48 +0200

    dayjs introduced

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import withStyles from '@material-ui/styles/withStyles';
-import Link from 'react-router-dom/Link';
+import {Link} from 'react-router-dom';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
@@ -23,13 +25,14 @@ const styles = {
 };
 export class Scream extends Component {
     render() {
+        dayjs.extend(relativeTime)
         const { classes, scream : { body, createdAt, userImage, userHandle, screamId, likeCount, commentCount} } =this.props
         return (
            <Card className={classes.card}>
            <CardMedia image={userImage} title="Profile image" className={classes.image}/>
-           <CardContent class={classes.content}>
+           <CardContent className={classes.content}>
             <Typography variant='h5' component={Link} to={`/users/${userHandle}`} color='primary' >{userHandle} </Typography>
-            <Typography variant='body2' color="textSecondary">{createdAt} </Typography>
+            <Typography variant='body2' color="textSecondary">{dayjs(createdAt).fromNow()} </Typography>
             <Typography variant='body1'>{body} </Typography>
            </CardContent>
            </Card>
