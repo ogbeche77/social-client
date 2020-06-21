ac456baedeed994613575500e12ca75f032f0411
ogbeche77 <ogbeche77@yahoo.com>
2020-06-21 17:46:25 +0200

    restructured

--- src/pages/home.js
+++ src/pages/home.js
@@ -12,7 +12,6 @@ export class home extends Component {
     componentDidMount() {
         axios.get('/scream')
         .then(res=> {
-            console.log(res.data)
             this.setState({
                 screams:res.data
             })
@@ -22,9 +21,9 @@ export class home extends Component {
         
     }
     render() {
-        let recentScreamsMarkup = this.state.screams ? (this.state.screams.map((scream) => <Scream scream={scream}/>)) : <p> Still loading</p>
+        let recentScreamsMarkup = this.state.screams ? (this.state.screams.map((scream) => <Scream key={scream.screamId} scream={scream}/>)) : <p> Still loading</p>
         return (
-            <Grid container spacing={16}>
+            <Grid container spacing={10}>
             <Grid item sm={8} xs={12}> 
             {recentScreamsMarkup}
             </Grid>
