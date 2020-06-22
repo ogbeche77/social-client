e8488eccf7cc385172dab9db77928f1c1b49a6d3
ogbeche77 <ogbeche77@yahoo.com>
2020-06-22 13:26:29 +0200

    login grid

--- src/pages/login.js
+++ src/pages/login.js
@@ -5,13 +5,17 @@ import myIcon from '../images/images.png';
 
 //MUI
 import Grid from '@material-ui/core/Grid';
+import { Typography } from '@material-ui/core';
 
 
 const styles = {
     form: {
         textAlign: 'center'
+    },
+    image: {
+        margin: '20px auto 20px auto'  //spacing btween navbar and icon image
     }
-}
+};
 
 export class login extends Component {
     render() {
@@ -19,7 +23,8 @@ export class login extends Component {
         return (
             <Grid container className={classes.form}>
             <Grid item sm> </Grid>
-            <Grid item sm><img src={myIcon} alt="socialcode"/> </Grid>
+            <Grid item sm><img src={myIcon} alt="socialcode" className={classes.image}/>
+            <Typography variant='h4' className={classes.pageTitle}> Login </Typography> </Grid>
             <Grid item sm> </Grid>
             </Grid>
         )
