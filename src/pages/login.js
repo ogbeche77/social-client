12551d35b5553f1e52610d11baac2754b777712a
ogbeche77 <ogbeche77@yahoo.com>
2020-06-22 13:10:09 +0200

    login functionality

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,13 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import withStyles from '@material-ui/core/styles/withStyles';
+import PropTypes from 'prop-types';
+
+//MUI
+import Grid from '@material-ui/core/Grid';
+
+
+const styles = {
+
+}
 
 export class login extends Component {
     render() {
+        const { classes } = this.props;
         return (
-            <div>
-                <h1>login Page</h1>
-            </div>
+            <Grid container className={classes.form}>
+            <Grid item sm> </Grid>
+            <Grid item sm><p>My business</p> </Grid>
+            <Grid item sm> </Grid>
+            </Grid>
         )
     }
 }
 
-export default login
+login.propTypes = {
+    classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(login);
