48fc9a9632759a355814d8d2cf986b2121ebf88a
ogbeche77 <ogbeche77@yahoo.com>
2020-06-22 13:45:59 +0200

    email/password form

--- src/pages/login.js
+++ src/pages/login.js
@@ -5,7 +5,7 @@ import myIcon from '../images/images.png';
 
 //MUI
 import Grid from '@material-ui/core/Grid';
-import { Typography } from '@material-ui/core';
+import { Typography, TextField } from '@material-ui/core';
 
 
 const styles = {
@@ -17,14 +17,41 @@ const styles = {
     }
 };
 
+
+
 export class login extends Component {
+    constructor(){
+        super();
+        this.state = {
+            email: '', 
+            password: "",
+            loading: false,
+            errors: {}
+        }
+    }
+
+    handleSubmit = (event) => {
+        console.log('hey')
+    }
+    handleChange = (event)=> {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
     render() {
         const { classes } = this.props;
         return (
             <Grid container className={classes.form}>
             <Grid item sm> </Grid>
             <Grid item sm><img src={myIcon} alt="socialcode" className={classes.image}/>
-            <Typography variant='h4' className={classes.pageTitle}> Login </Typography> </Grid>
+            <Typography variant='h4' className={classes.pageTitle}> Login </Typography>
+            <form noValidate onSubmit={this.handleSubmit}>
+            <TextField id='email' name='email' type='email' label="Email" className={classes.textField}
+            value={this.state.email} onChange={this.handleChange} fullWidth/> 
+            <TextField id='password' name='password' type='password' label="Password" className={classes.textField}
+            value={this.state.email} onChange={this.handleChange} fullWidth/>
+            </form> 
+            </Grid>
             <Grid item sm> </Grid>
             </Grid>
         )
