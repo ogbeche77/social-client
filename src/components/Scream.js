72cff3b096fd39f340612c80f61a82a38d552dc2
ogbeche77 <ogbeche77@yahoo.com>
2020-06-28 07:51:37 +0200

    card & card content added

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -3,11 +3,7 @@ import withStyles from '@material-ui/styles/withStyles';
 import {Link} from 'react-router-dom';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
-
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import { Typography } from '@material-ui/core';
+import { Typography, Card, CardContent, CardMedia } from '@material-ui/core';
 
 const styles = {
     card: {
